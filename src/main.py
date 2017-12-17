7bf678f48c1a5009a4d98602705f0774688e6490
Narbeh <narbeh@protonmail.com>
2017-12-17 11:39:41 +0330

    Refactor Code As PEP8

--- src/main.py
+++ src/main.py
@@ -19,30 +19,34 @@ myAccount.login()
 
 # Function To display Owner's Details
 def DisplayOwnerAccountInfo():
-  print ("Your Account Information :")
-  data = myAccount.getMyUserInfo()
-  for key, value in data.items():
-      print (key,value)
+    print ("Your Account Information :")
+    data = myAccount.getMyUserInfo()
+    for key, value in data.items():
+        print (key, value)
+
 
 # DisplayOwnerAccountInfo()
+
 
 # Function to Display Followers
 def DisplayMyFollowers():
     print ("They are Following You:")
     print ("========================")
     for followers in myAccount.getMyFollowers():
-      print(followers.screen_name)
+        print(followers.screen_name)
     print ("========================")
+
 
 # DisplayMyFollowers()
 
+
 # Function to List the users that you Follow
 def DisplayFollowingMe():
-  print ("You are Following:")
-  print ("========================")
-  for following in myAccount.getMyFollowings():
-    print(following.screen_name)
-  print ("========================")
+    print ("You are Following:")
+    print ("========================")
+    for following in myAccount.getMyFollowings():
+        print(following.screen_name)
+    print ("========================")
 
 # DisplayFollowingMe()
 
@@ -50,26 +54,27 @@ def DisplayFollowingMe():
 # myAccount.follow('smm_taheri')
 # myAccount.unfollow('smm_taheri')
 
+
 def helpCommand():
-  print("""Enter "myaccountinfo" to show the information of your account""")
-  print("""Enter "myfollowers" to show your followers""")
-  print("""Enter "myfollowings" to show your followings""")
+    print("""Enter "myaccountinfo" to show the information of your account""")
+    print("""Enter "myfollowers" to show your followers""")
+    print("""Enter "myfollowings" to show your followings""")
 
 def main():
-  print("***Welcome to CLI mode***")
-  print("""Enter "help" to show list of commands or Enter "exit" to exit the cli mode!""")
-  print(">>> ", end="")
-  command = input()
-  while str(command) != "exit":
-    if command == "help":
-      helpCommand()
-    elif command == "myaccountinfo":
-      DisplayOwnerAccountInfo()
-    elif command == "myfollowers":
-      DisplayMyFollowers()
-    elif command == "myfollowings":
-      DisplayFollowingMe()
-    print(">>> ", end='')
+    print("***Welcome to CLI mode***")
+    print("""Enter "help" to show list of commands or Enter "exit" to exit the cli mode!""")
+    print(">>> ", end="")
     command = input()
+    while str(command) != "exit":
+        if command == "help":
+            helpCommand()
+        elif command == "myaccountinfo":
+            DisplayOwnerAccountInfo()
+        elif command == "myfollowers":
+            DisplayMyFollowers()
+        elif command == "myfollowings":
+            DisplayFollowingMe()
+        print(">>> ", end='')
+        command = input()
 
 main()
